6be136da552e4708e0fb0bd5943cedaac108e295
Andrei Melchikov <andrei.melchikov25@gmail.com>
2026-04-14 21:01:22 +0400

    feat(boot): add generated assets and boot scene

---
+++ src/game/scenes/BootScene.ts
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+import { HighscoreStore } from "../services/HighscoreStore";
+import { SCENE_KEYS } from "../types/scene";
+import { GAME_TITLE, WORLD_HEIGHT, WORLD_WIDTH } from "../utils/constants";
+import { generateTextures } from "../utils/textureFactory";
+
+export class BootScene extends Phaser.Scene {
+  public constructor() {
+    super(SCENE_KEYS.BOOT);
+  }
+
+  public create(): void {
+    generateTextures(this);
+    HighscoreStore.migrateLegacyScores();
+
+    this.registry.set("gameTitle", GAME_TITLE);
+    this.registry.set("worldWidth", WORLD_WIDTH);
+    this.registry.set("worldHeight", WORLD_HEIGHT);
+
+    this.scene.start(SCENE_KEYS.MENU);
+  }
+}
