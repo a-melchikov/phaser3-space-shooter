98034e87fdd397b55da35dc65306aaa6f1a9f0e1
Andrei Melchikov <andrei.melchikov25@gmail.com>
2026-04-14 21:01:08 +0400

    chore: initialize phaser vite typescript project

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: currentDir
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        localStorage: "readonly",
+        performance: "readonly",
+        HTMLElement: "readonly",
+        HTMLCanvasElement: "readonly",
+        MouseEvent: "readonly",
+        KeyboardEvent: "readonly"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_"
+        }
+      ]
+    }
+  }
+];
