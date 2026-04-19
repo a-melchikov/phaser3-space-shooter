7fd452c633e1abed5b1bedf311563c7418349304
Andrei Melchikov <andrei.melchikov25@gmail.com>
2026-04-19 15:38:19 +0400

    perf(build): split phaser into dedicated vendor chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/phaser")) {
+            return "phaser";
+          }
+        }
+      }
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: 5173
